e24db1d48eaa07cb07dd07dd3b861e4ffd6d9d3f
Leonardo Figueira Gomes Freire <leonardo.freire@glintt.com>
2021-01-19 00:30:36 +0000

    Initial setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3fb39f4c5092412738b0a792167f30a'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4e19378b-8e85-487e-8a30-4698097a166a'
+};
