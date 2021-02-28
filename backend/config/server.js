325f60f891910d26d4e8165a57cd916847fe451b
Leonardo Figueira Gomes Freire <leonardo.freire@glintt.com>
2021-02-28 00:41:10 +0000

    Server config changes

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,8 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a3fb39f4c5092412738b0a792167f30a'),
-    },
+    url: '/dashboard', // We change the path to access to the admin (highly recommended for security reasons).
+    // auth: {
+    //   secret: env('ADMIN_JWT_SECRET', 'a3fb39f4c5092412738b0a792167f30a'),
+    // },
   },
 });
